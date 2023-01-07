66553666c6baa41566623c7c259b4931bb824e94
azufb <curiousazunas@gmail.com>
2023-01-07 20:49:23 +0900

    useStateの実践

--- src/components/hooks/StateHooks.tsx
+++ src/components/hooks/StateHooks.tsx
@@ -1,7 +1,50 @@
+import React, { useState } from 'react';
+
 const StateHooks = () => {
+    const [age, setAge] = useState<number>(24);
+    const [name, setName] = useState<string>('名無し');
+    const [inputName, setInputName] = useState<string>('');
+
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputName(e.target.value);
+    }
+
+    const handleSetName = () => {
+        setName(inputName);
+    };
+
+    const increment = () => {
+        setAge(currentAge => currentAge + 1);
+    };
+
+    const decrement = () => {
+        setAge(currentAge => currentAge - 1);
+    }
+
     return (
         <div>
             <h1>State Hooksについて</h1>
+            <div>
+                <div>
+                    <label>お名前：</label>
+                    <span>{name}</span>
+
+                    <div>
+                        <input type='text' value={inputName} onChange={(e: any) => onChangeInput(e)} />
+                        <button onClick={handleSetName}>送信</button>
+                    </div>
+                </div>
+
+                <div>
+                    <label>年齢：</label>
+                    <span>{age}</span>
+
+                    <div>
+                        <button onClick={increment}>1歳足す</button>
+                        <button onClick={decrement}>1歳引く</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
